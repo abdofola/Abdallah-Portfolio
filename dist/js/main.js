97d1e32d7772a6517a9dc1800caf125becceed7a
fola <abdofola67@gmail.com>
2021-05-12 02:38:21 +0200

    Add responsiveness

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,12 +3,13 @@ const menuBtnToggler = document.querySelector('.menu-btn__burger');
 const nav = document.querySelector('.nav');
 const menuNav = document.querySelector('.menu-nav');
 const menuItem = document.querySelectorAll('.menu-nav__item');
+const bioImg = document.querySelector('.projects__bio-image');
 let open = false;
 
 // Event for toggling the menu
 menuBtn.addEventListener('click', menuToggle);
+
 function menuToggle() {
-    console.log(this);
     if (!open) {
         menuBtnToggler.classList.add('open');
         nav.classList.add('open');
@@ -23,3 +24,14 @@ function menuToggle() {
         open = false;
     }
 }
+
+function callback() {
+    const bioRec = bioImg.getBoundingClientRect();
+
+    (bioRec.bottom <= 100) ?
+        nav.classList.add('scroll')
+        : nav.classList.remove('scroll')
+
+    window.requestAnimationFrame(callback);
+}
+window.requestAnimationFrame(callback);
