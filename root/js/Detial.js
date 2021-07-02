c32ee951881edf93dadcbaf483594af85d87e1cd
fola <abdofola67@gmail.com>
2021-07-02 10:27:28 +0200

    Change private class fields cuz browsers support

--- root/js/Detial.js
+++ root/js/Detial.js
@@ -4,30 +4,31 @@
 
 export default class DetialModel {
   // Data
-  #title;
-  #screenShots;
-  #about;
-  #technical;
+  // Heads-up -- Private fields have fiewer support across browsers
+  // #title;
+  // #screenShots;
+  // #about;
+  // #technical;
 
   // Constructor
   constructor(title, screenShots, about, technical) {
-    this.#title = title;
-    this.#screenShots = screenShots;
-    this.#about = about;
-    this.#technical = technical;
+    this.title = title;
+    this.screenShots = screenShots;
+    this.about = about;
+    this.technical = technical;
   }
 
   // Methods
   getTitle() {
-    return this.#title;
+    return this.title;
   }
   getScreens() {
-    return this.#screenShots;
+    return this.screenShots;
   }
   getAbout() {
-    return this.#about;
+    return this.about;
   }
   getTech() {
-    return this.#technical;
+    return this.technical;
   }
 }
